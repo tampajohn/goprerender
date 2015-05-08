060e26b446ea950cb084665b8776d24f5687b018
Ian Bishop <ianbishop@pace7.com>
2015-05-08 14:08:42 +1000

    Remove console debug

--- prerender.go
+++ prerender.go
@@ -4,7 +4,6 @@ package prerender
 
 import (
 	"compress/gzip"
-	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -51,7 +50,6 @@ func (o *Options) NewPrerender() *Prerender {
 
 // ServeHTTP allows Prerender to act as a Negroni middleware.
 func (p *Prerender) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	fmt.Println("Prerender")
 	if p.ShouldPrerender(r) {
 		p.PreRenderHandler(rw, r)
 	} else if next != nil {
@@ -62,7 +60,6 @@ func (p *Prerender) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http
 // ShouldPrerender analyzes the request to determine whether it should be routed
 // to a Prerender.io upstream server.
 func (p *Prerender) ShouldPrerender(or *http.Request) bool {
-	fmt.Println(or)
 	userAgent := strings.ToLower(or.Header.Get("User-Agent"))
 	bufferAgent := or.Header.Get("X-Bufferbot")
 	isRequestingPrerenderedPage := false
@@ -177,7 +174,6 @@ func (p *Prerender) PreRenderHandler(rw http.ResponseWriter, or *http.Request) {
 
 	res, err := client.Do(req)
 
-	fmt.Println(res)
 	e.Check(err)
 
 	rw.Header().Set("Content-Type", res.Header.Get("Content-Type"))
